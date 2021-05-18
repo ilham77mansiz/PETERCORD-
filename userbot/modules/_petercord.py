adbfff9000542ecd189dd8d51329504bb1ad5ce6
ilham77mansiz <80214676+ilham77mansiz@users.noreply.github.com>
2021-05-19 00:00:14 +0700

    Create _petercord.py

---
+++ userbot/modules/_petercord.py
@@ -0,0 +1,29 @@
+from userbot.cmdhelp import CmdHelp
+from userbot import cmdhelp
+from userbot import CMD_HELP
+from userbot.events import register
+
+
+@register(outgoing=True, pattern="^.petercordhelp(?: |$)(.*)")
+async def master(event):
+    """ .master ketik """
+    args = event.pattern_match.group(1).lower()
+    if args:
+        if args in CMD_HELP:
+            await event.edit(str(CMD_HELP[args]))
+        else:
+            await event.edit(Tolong..)
+    else:
+        string = ""
+        sayfa = [sorted(list(CMD_HELP))[i:i + 5] for i in range(0, len(sorted(list(CMD_HELP))), 5)]
+        
+        for i in sayfa:
+            string += f'`⚡ `'
+            for sira, a in enumerate(i):
+                string += "`" + str(a)
+                if sira == i.index(i[-1]):
+                    string += "`"
+                else:
+                    string += "`, "
+            string += "\n"
+        await event.edit("PETERCORD" + '\n\n' + string)
