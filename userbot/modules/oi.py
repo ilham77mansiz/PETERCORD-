f82337761a583f6446be223f36aad8213fa41f7d
Mr.Miss <keselekpermen69@gmail.com>
2021-05-18 15:52:27 +0000

    pylint: auto fixes

--- userbot/modules/oi.py
+++ userbot/modules/oi.py
@@ -1,7 +1,7 @@
 from time import sleep
 from userbot.events import register
 from userbot.cmdhelp import CmdHelp
-from userbot import CMD_HELP
+
 
 @register(outgoing=True, pattern='^.ilham(?: |$)(.*)')
 async def typewriter(typew):
@@ -40,6 +40,5 @@ async def typewriter(typew):
     await typew.edit("`Dan Selalu Bersyukur`")
 # Create by myself @localheart
 
-CmdHelp('adzan').add_command(
-    'adzan', '<kota> <daerah>', 'Untuk menampilkan waktu sholat di kota yang telah di tentukan.'
-).add()
+CmdHelp('adzan').add_command('adzan', '<kota> <daerah>',
+                             'Untuk menampilkan waktu sholat di kota yang telah di tentukan.').add()
