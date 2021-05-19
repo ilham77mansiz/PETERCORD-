f5c646c5bb1f087a07ea0d94751b23a72a8c1f51
ilham77mansiz <80214676+ilham77mansiz@users.noreply.github.com>
2021-05-19 17:02:38 +0700

    Update lock.py

--- userbot/modules/lock.py
+++ userbot/modules/lock.py
@@ -9,7 +9,7 @@ from telethon.tl.types import ChatBannedRights
 
 from userbot import CMD_HELP
 from userbot.events import register
-
+from userbot.cmdhelp import CmdHelp
 
 @register(outgoing=True, pattern=r"^\.lock ?(.*)")
 async def locks(event):
@@ -187,11 +187,14 @@ async def rem_locks(event):
         return
 
 
-CMD_HELP.update({
-    "locks":
-    "`.lock <all atau Jenis>` atau `.unlock <all atau Jenis>`\
-\nUsage: Memungkinkan anda kunci atau membuka kunci, beberapa jenis pesan dalam obrolan.\
-\n[Anda Harus Jadi Admin Grup Untuk Menggunakan Perintah!]\
-\n\nJenis pesan yang bisa dikunci atau dibuka adalah: \
-\n`all, msg, media, sticker, gif, game, inline, poll, invite, pin, info`\n**Contoh:** `.lock msg` atau `.unlock msg`"
-})
+CmdHelp('lock').add_command(
+    'filters', None, 'Bir sohbetteki tüm userbot filtrelerini listeler.'
+).add_command(
+    'filter', '<filtrelenecek kelime> <cevaplanacak metin> ya da bir mesajı .filter <filtrelenecek kelime>', 'Filtre ekler. Ne zaman eklediğiniz kelime/cümle yazılırsa bot cevap verir.', '.filter "merhaba" "meraba"'
+).add_command(
+    'stop', '<filtre>', 'Seçilen filtreyi durdurur.'
+).add_command(
+    'genelfilter', '<filtrelenecek kelime> <cevaplanacak metin> ya da bir mesajı .genelfilter <filtrelenecek kelime>', 'Genel filtre ekler. Tüm gruplarda çalışır.'
+).add_command(
+    '.genelstop', '<filtre>', 'Seçilen genel filtreyi durdurur.'
+).add()
