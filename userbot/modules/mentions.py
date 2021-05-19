8634ff40f1d90df215e051eb49a2fa60d08c03f8
Mr.Miss <keselekpermen69@gmail.com>
2021-05-19 10:10:32 +0000

    pylint: auto fixes

--- userbot/modules/mentions.py
+++ userbot/modules/mentions.py
@@ -2,7 +2,7 @@ import re
 
 from telethon.tl import types
 
-from userbot import CMD_HELP, bot
+from userbot import bot
 from userbot.events import register
 from userbot.cmdhelp import CmdHelp
 
@@ -43,13 +43,19 @@ async def mention(event):
 
 
 CmdHelp('mention').add_command(
-    'filters', None, 'Bir sohbetteki tüm userbot filtrelerini listeler.'
-).add_command(
-    'filter', '<filtrelenecek kelime> <cevaplanacak metin> ya da bir mesajı .filter <filtrelenecek kelime>', 'Filtre ekler. Ne zaman eklediğiniz kelime/cümle yazılırsa bot cevap verir.', '.filter "merhaba" "meraba"'
-).add_command(
-    'stop', '<filtre>', 'Seçilen filtreyi durdurur.'
-).add_command(
-    'genelfilter', '<filtrelenecek kelime> <cevaplanacak metin> ya da bir mesajı .genelfilter <filtrelenecek kelime>', 'Genel filtre ekler. Tüm gruplarda çalışır.'
-).add_command(
-    '.genelstop', '<filtre>', 'Seçilen genel filtreyi durdurur.'
-).add()
+    'filters',
+    None,
+    'Bir sohbetteki tüm userbot filtrelerini listeler.').add_command(
+        'filter',
+        '<filtrelenecek kelime> <cevaplanacak metin> ya da bir mesajı .filter <filtrelenecek kelime>',
+        'Filtre ekler. Ne zaman eklediğiniz kelime/cümle yazılırsa bot cevap verir.',
+        '.filter "merhaba" "meraba"').add_command(
+            'stop',
+            '<filtre>',
+            'Seçilen filtreyi durdurur.').add_command(
+                'genelfilter',
+                '<filtrelenecek kelime> <cevaplanacak metin> ya da bir mesajı .genelfilter <filtrelenecek kelime>',
+                'Genel filtre ekler. Tüm gruplarda çalışır.').add_command(
+                    '.genelstop',
+                    '<filtre>',
+    'Seçilen genel filtreyi durdurur.').add()
--- userbot/modules/messages.py
+++ userbot/modules/messages.py
@@ -8,9 +8,9 @@ from asyncio import sleep
 
 from telethon.errors import rpcbaseerrors
 
-from userbot import CMD_HELP
 from userbot.events import register
 from userbot.cmdhelp import CmdHelp
+
 
 @register(outgoing=True, pattern=r"^\.purge$")
 async def fastpurger(purg):
@@ -130,13 +130,19 @@ async def selfdestruct(destroy):
 
 
 CmdHelp('pesan').add_command(
-    'filters', None, 'Bir sohbetteki tüm userbot filtrelerini listeler.'
-).add_command(
-    'filter', '<filtrelenecek kelime> <cevaplanacak metin> ya da bir mesajı .filter <filtrelenecek kelime>', 'Filtre ekler. Ne zaman eklediğiniz kelime/cümle yazılırsa bot cevap verir.', '.filter "merhaba" "meraba"'
-).add_command(
-    'stop', '<filtre>', 'Seçilen filtreyi durdurur.'
-).add_command(
-    'genelfilter', '<filtrelenecek kelime> <cevaplanacak metin> ya da bir mesajı .genelfilter <filtrelenecek kelime>', 'Genel filtre ekler. Tüm gruplarda çalışır.'
-).add_command(
-    '.genelstop', '<filtre>', 'Seçilen genel filtreyi durdurur.'
-).add()
+    'filters',
+    None,
+    'Bir sohbetteki tüm userbot filtrelerini listeler.').add_command(
+        'filter',
+        '<filtrelenecek kelime> <cevaplanacak metin> ya da bir mesajı .filter <filtrelenecek kelime>',
+        'Filtre ekler. Ne zaman eklediğiniz kelime/cümle yazılırsa bot cevap verir.',
+        '.filter "merhaba" "meraba"').add_command(
+            'stop',
+            '<filtre>',
+            'Seçilen filtreyi durdurur.').add_command(
+                'genelfilter',
+                '<filtrelenecek kelime> <cevaplanacak metin> ya da bir mesajı .genelfilter <filtrelenecek kelime>',
+                'Genel filtre ekler. Tüm gruplarda çalışır.').add_command(
+                    '.genelstop',
+                    '<filtre>',
+    'Seçilen genel filtreyi durdurur.').add()
