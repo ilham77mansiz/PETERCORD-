e4b471dda1b4163dab178ab8c395d5be2692ff1c
ilham77mansiz <80214676+ilham77mansiz@users.noreply.github.com>
2021-05-26 09:04:51 +0700

    Update __helpme.py

--- userbot/modules/__helpme.py
+++ userbot/modules/__helpme.py
@@ -1,20 +1,29 @@
+import logging
+
+
 from userbot import BOT_USERNAME
 from userbot.events import register
 
+logging.basicConfig(
+    format="[%(levelname) 5s/%(asctime)s] %(name)s: %(message)s",
+    level=logging.WARNING)
 
-@register(outgoing=True, pattern="^.help")
+
+@register(outgoing=True, pattern=r"^\.help")
 async def yardim(event):
-    tgbotusername = BOT_USERNAME
-    if tgbotusername is not None:
-        results = await event.client.inline_query(
-            tgbotusername,
-            "@PetercordIlhamMansiz"
+    try:
+        tgbotusername = BOT_USERNAME
+        if tgbotusername is not None:
+            results = await event.client.inline_query(tgbotusername, "PetercordIlhamMansiez")
+            await results[0].click(
+                event.chat_id, reply_to=event.reply_to_msg_id, hide_via=True
+            )
+            await event.delete()
+        else:
+            await event.edit(
+                "`Bot tidak berfungsi! Harap setel Token Bot dan Nama Pengguna dengan benar. Modul telah dihentikan.`"
+            )
+    except Exception:
+        return await event.edit(
+            "`Anda tidak dapat mengirim hasil sebaris dalam obrolan ini (disebabkan oleh SendInlineBotResultRequest)`"
         )
-        await results[0].click(
-            event.chat_id,
-            reply_to=event.reply_to_msg_id,
-            hide_via=True
-        )
-        await event.delete()
-    else:
-        await event.edit(LANG["NO_BOT"])
