33c5e9c3c144069a1b4550b5cfefc5775d2f7445
Mr.Miss <keselekpermen69@gmail.com>
2021-05-26 01:55:25 +0000

    pylint: auto fixes

--- userbot/cmdhelp.py
+++ userbot/cmdhelp.py
@@ -15,35 +15,48 @@ class CmdHelp:
     WARNING = ""
     INFO = ""
 
-    def __init__(self, file: str, official : bool = True, file_name : str = None):
+    def __init__(
+            self,
+            file: str,
+            official: bool = True,
+            file_name: str = None):
         self.FILE = file
         self.ORIGINAL_FILE = file
         self.IS_OFFICIAL = official
-        self.FILE_NAME = file_name if not file_name == None else file + '.py'
+        self.FILE_NAME = file_name if file_name is not None else file + '.py'
         self.COMMANDS = {}
         self.FILE_AUTHOR = ""
         self.WARNING = ""
         self.INFO = ""
 
-    def set_file_info(self, name : str, value : str):
+    def set_file_info(self, name: str, value: str):
         if name == 'name':
             self.FILE = value
         elif name == 'author':
             self.FILE_AUTHOR = value
         return self
-        
-    def add_command(self, command : str, params = None, usage: str = '', example = None):
+
+    def add_command(
+            self,
+            command: str,
+            params=None,
+            usage: str = '',
+            example=None):
         """
         Komut ekler.
         """
-        
-        self.COMMANDS[command] = {'command': command, 'params': params, 'usage': usage, 'example': example}
+
+        self.COMMANDS[command] = {
+            'command': command,
+            'params': params,
+            'usage': usage,
+            'example': example}
         return self
-    
+
     def add_warning(self, warning):
         self.WARNING = warning
         return self
-    
+
     def add_info(self, info):
         self.INFO = info
         return self
@@ -58,7 +71,7 @@ class CmdHelp:
             result += f"**🎖PERINTAH PETERCORD:** {'⚡' if self.IS_OFFICIAL else '❌'}\n\n"
         else:
             result += f"**🎖PERINTAH PETERCORD:** {'⚡' if self.IS_OFFICIAL else '❌'}\n"
-            
+
             if self.INFO == '':
                 if not self.WARNING == '':
                     result += f"**❌ Berbahaya:** {self.WARNING}\n\n"
@@ -66,15 +79,15 @@ class CmdHelp:
                 if not self.WARNING == '':
                     result += f"**❌ Berbahaya:** {self.WARNING}\n"
                 result += f"**🎖INFORMASI:** {self.INFO}\n\n"
-                     
+
         for command in self.COMMANDS:
             command = self.COMMANDS[command]
-            if command['params'] == None:
+            if command['params'] is None:
                 result += f"**🎖DAFTAR PETERCORD:** `{PATTERNS[:1]}{command['command']}`\n"
             else:
                 result += f"**🎖PERINTAH PETERCORD:** `{PATTERNS[:1]}{command['command']} {command['params']}`\n"
-                
-            if command['example'] == None:
+
+            if command['example'] is None:
                 result += f"**🎖PERINTAH PETERCORD:** `{command['usage']}`\n\n"
             else:
                 result += f"**🎖PESAN:** `{command['usage']}`\n"
@@ -85,11 +98,11 @@ class CmdHelp:
         """
         Direkt olarak CMD_HELP ekler.
         """
-        CMD_HELP_BOT[self.FILE] = {'info': {'official': self.IS_OFFICIAL, 'warning': self.WARNING, 'info': self.INFO}, 'commands': self.COMMANDS}
+        CMD_HELP_BOT[self.FILE] = {'info': {'official': self.IS_OFFICIAL,
+                                            'warning': self.WARNING, 'info': self.INFO}, 'commands': self.COMMANDS}
         CMD_HELP[self.FILE] = self.get_result()
         return True
-    
-    
+
     def getText(self, text: str):
         if text == 'REPLY_OR_USERNAME':
             return '<nama pengguna> <username / reply>'
